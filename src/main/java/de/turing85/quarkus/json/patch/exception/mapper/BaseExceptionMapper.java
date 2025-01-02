ae14eadea183133f7c3633db293cb77d07bc1f95
Marco Bungart <marco.bungart@googlemail.com>
2025-01-02 01:20:29 +0100

    Refactoring

--- src/main/java/de/turing85/quarkus/json/patch/exception/mapper/BaseExceptionMapper.java
+++ src/main/java/de/turing85/quarkus/json/patch/exception/mapper/BaseExceptionMapper.java
@@ -3,10 +3,11 @@ package de.turing85.quarkus.json.patch.exception.mapper;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import org.jboss.logging.Logger;
 
-@RequiredArgsConstructor
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class BaseExceptionMapper<T extends Throwable> implements ExceptionMapper<T> {
   private final Logger logger;
 
@@ -22,8 +23,8 @@ public abstract class BaseExceptionMapper<T extends Throwable> implements Except
 
   @Override
   public final Response toResponse(T throwable) {
-    // @formatter:off
     logger().logf(level(), throwable, "Caught exception, responding with status %s", status());
+    // @formatter:off
     return Response
         .status(status())
         .entity(Error.builder().message(throwable.getMessage()).build())
