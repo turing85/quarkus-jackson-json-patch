21d3da98f8a7c3bdd93f6ab22e043b8ffd94a03e
Marco Bungart <marco.bungart@googlemail.com>
2025-01-11 17:42:10 +0100

    Move schema definitions to DTOs where possible

--- src/main/java/de/turing85/quarkus/json/patch/exception/mapper/ErrorResponse.java
+++ src/main/java/de/turing85/quarkus/json/patch/exception/mapper/ErrorResponse.java
@@ -2,16 +2,23 @@ package de.turing85.quarkus.json.patch.exception.mapper;
 
 import java.util.Optional;
 
+import jakarta.validation.constraints.NotNull;
+
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Value;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.jspecify.annotations.Nullable;
 
+@Schema(name = ErrorResponse.SCHEMA_NAME)
 @Value
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @RegisterForReflection
 public class ErrorResponse {
+  public static final String SCHEMA_NAME = "Error";
+
+  @NotNull
   String message;
 
   public static ErrorResponse of(@Nullable final String message) {
